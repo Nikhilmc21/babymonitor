5f1f6770c49371d884c467ee44c77f0fdad5445e
Nikhilmc21 <84916563+Nikhilmc21@users.noreply.github.com>
2025-05-21 16:51:08 +0530

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -56,7 +56,7 @@ export async function execute(message) {
 
   if (!data[userId]) {
     data[userId] = {
-      username: message.author.username
+      username: message.author.username,
       count: 0,
       date: getTodayDateStr(),
       timeoutUntil: 0
